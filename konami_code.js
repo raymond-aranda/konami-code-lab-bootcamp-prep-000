7a4124fba6349dcd966f9bd07d0a0e5a809d9364
Raymond Aranda <raymond.aranda@gmail.com>
2017-09-26 23:32:52 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  document.body.addEventListener('keydown', test)
+document.body.addEventListener('keydown', init);
+
+var index = 0;
+
+function init(e) {
+  const key = parseInt(e.detail || e.which);
+
+  if (index === code[index]) {
+    index++;
+
+    if (index === code.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 
 }
